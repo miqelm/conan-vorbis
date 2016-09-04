72ec2d141ab3afcbadac33af6aaecd06d8972a75
dimitri <dimi309@users.noreply.github.com>
2016-09-04 14:12:05 +0200

    On Windows, only building for VS2015.

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ import os
 username = os.getenv("CONAN_USERNAME", "coding3d")
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username, archs =  ["x86_64"], gcc_versions =  ["4.6", "4.8", "4.9", "5.2"])
+    builder = ConanMultiPackager(username=username, archs =  ["x86_64"], gcc_versions =  ["4.6", "4.8", "4.9", "5.2"], visual_versions = [14])
     builder.add_common_builds()
     builder.run()
