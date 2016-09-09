e2ded8d99150df567b57f28f0f4b7d19144fab6b
dimitri <dimi309@users.noreply.github.com>
2016-09-10 00:12:07 +0200

    64-bit builds

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(visual_versions=[14])
+    builder = ConanMultiPackager(archs=['x86_64'], visual_versions=[14])
     builder.add_common_builds(pure_c=True)
     builder.run()
