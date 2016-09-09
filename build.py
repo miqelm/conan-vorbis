f892be07b4e32833551a74503f8e0a057990232a
dimitri <dimi309@users.noreply.github.com>
2016-09-09 05:02:16 +0200

    32-bit build

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"], args=['--build', 'all'])
+    builder = ConanMultiPackager(archs=["x86"], args="--build ogg")
     builder.add_common_builds(pure_c=True)
     builder.run()
