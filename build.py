61fcfe410461dcdb959119d62e9ae6909ed546df
dimitri <dimi309@users.noreply.github.com>
2016-09-09 02:06:43 +0200

    Removed username from build.py

--- build.py
+++ build.py
@@ -1,9 +1,7 @@
 from conan.packager import ConanMultiPackager
 import os
 
-username = os.getenv("CONAN_USERNAME", "coding3d")
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username)
-    builder.add_common_builds(pure_c=True)
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="vorbis:shared", pure_c=True)
     builder.run()
