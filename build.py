acd4851fd0196bd58c19936f1eebc9183e6026d6
dimitri <dimi309@users.noreply.github.com>
2016-09-09 04:50:35 +0200

    Build all deps

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"])
+    builder = ConanMultiPackager(archs=["x86_64"], args=['--build', 'all'])
     builder.add_common_builds(pure_c=True)
     builder.run()
